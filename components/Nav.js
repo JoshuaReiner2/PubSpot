aa536cd5230f43946749aa86cfe3ce725f489fbf
JoshuaReiner2 <joshua.reiner2@gmail.com>
2026-06-09 10:45:06 -0400

    Add Clerk authentication

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,30 @@
 import Link from 'next/link'
+import { useUser, UserButton, SignInButton } from '@clerk/nextjs'
 import styles from './Nav.module.css'
 
 export default function Nav() {
+  const { isSignedIn } = useUser()
+
   return (
     <nav className={styles.nav}>
       <Link href="/" className={styles.logo}>
         Pub<span>Spot</span>
       </Link>
       <div className={styles.links}>
-        <Link href="/generate">Generate proposal</Link>
-        <Link href="/#pricing">Pricing</Link>
+        <Link href="/#how">How it works</Link>
+        {isSignedIn ? (
+          <>
+            <Link href="/generate">Generate proposal</Link>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        ) : (
+          <>
+            <SignInButton mode="modal">
+              <button className={styles.signInBtn}>Sign in</button>
+            </SignInButton>
+            <Link href="/sign-up">Get started</Link>
+          </>
+        )}
       </div>
     </nav>
   )
